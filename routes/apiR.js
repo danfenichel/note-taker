4da7fbdeb53decd28afd28d17531c706701e2052
D0491 <dan.fenichel@yahoo.com>
2020-01-25 11:15:48 -0600

    added route to html and api router files

--- routes/apiR.js
+++ routes/apiR.js
@@ -1,3 +1,25 @@
 const router = require("express").Router();
-const db = require("../db/store");
+const store = require("../db/store");
 
+router.get("/notes", function(req, res){
+    store
+    .getNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+});
+
+router.post("/notes", function(req, res){
+    store
+    .addNotes(req.body)
+    .then((note) => res.json(note))
+    .catch(err => res.status(500).json(err));
+});
+
+router.delete("/notes/:id", function(req, res){
+    store
+    .delNotes(req.params.id)
+    .then(() => res.json({ ok:true }))
+    .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
