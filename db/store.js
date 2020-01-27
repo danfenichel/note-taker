0061b24ac17f9acb2fb0c303918e4135bfdc10b2
D0491 <dan.fenichel@yahoo.com>
2020-01-26 18:30:45 -0600

    tried to fix routing files

--- db/store.js
+++ db/store.js
@@ -10,23 +10,40 @@ class Store {
     }
 
     read() {
-
+        return readFileA("./db.json", "utf8");
     }
 
     write(note) {
-
+        return writeFileA("./db.json", JSON.stringify(note));
     }
 
     getNotes() {
+        /////   * GET `/api/notes` 
+        //Should read the `db.json` file and return all saved notes as JSON.
+        this.read()
+            .then(notes => {
+                let parseNotes;
 
+                try {
+                    parseNotes = [].concat(JSON.parse(notes));
+                }
+                catch (err) {
+                    parseNotes = [];
+                }
+                return parseNotes;
+            });
     }
-
     addNotes(note) {
-
+        //   * POST `/api/notes` 
+        // - Should recieve a new note to save on the request body, add it to the `db.json` file, 
+        //and then return the new note to the client.
+        this.write(note)
+            .then(() => {
+                fs.appendFile()
+            })
     }
-
-    delNotes(id) {
-
+    deleteNotes(id) {
+        //   * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
     }
 }
 
--- routes/apiR.js
+++ routes/apiR.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 const store = require("../db/store");
 
-router.get("/notes", function(req, res){
+router.get("/api/notes", function(req, res){
     store
     .getNotes()
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 });
 
-router.post("/notes", function(req, res){
+router.post("/api/notes", function(req, res){
     store
     .addNotes(req.body)
     .then((note) => res.json(note))
     .catch(err => res.status(500).json(err));
 });
 
-router.delete("/notes/:id", function(req, res){
+router.delete("/api/notes/:id", function(req, res){
     store
     .delNotes(req.params.id)
     .then(() => res.json({ ok:true }))
