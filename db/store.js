bd6a90a8d0485b2eae9f57b4d3f33fd29b7c5e9e
D0491 <dan.fenichel@yahoo.com>
2020-01-27 15:04:46 -0600

    Fixed getNotes function

--- db/store.js
+++ db/store.js
@@ -8,7 +8,7 @@ class Store {
     constructor() {
         this.lastId = 0;
 
-        const data = fs.readFileA("./db.json", { encoding: "utf8" });
+        const data = fs.readFileSync("./db.json", { encoding: "utf8" });
         if(data.length > 0) {
             let currentNotes = JSON.parse(data);
             for (let i = 0; i < currentNotes.length; i++) {
@@ -29,9 +29,9 @@ class Store {
     }
 
     getNotes() {
-        /////   * GET `/api/notes` 
-        //Should read the `db.json` file and return all saved notes as JSON.
-        this.read()
+        // * GET `/api/notes` 
+        // Should read the `db.json` file and return all saved notes as JSON.
+        return this.read()
             .then(notes => {
                 let parseNotes;
 
@@ -45,16 +45,16 @@ class Store {
             });
     }
     addNotes(note) {
-        //   * POST `/api/notes` 
+        // * POST `/api/notes` 
         // - Should recieve a new note to save on the request body, add it to the `db.json` file, 
-        //and then return the new note to the client.
+        // and then return the new note to the client.
         this.write(note)
             .then(() => {
                 fs.appendFile()
             })
     }
     deleteNotes(id) {
-        //   * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+        // * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
     }
 }
 
